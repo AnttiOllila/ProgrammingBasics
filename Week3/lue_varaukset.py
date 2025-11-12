7c8f5995773e43f5dd58a044621c0be7571c10b5
AOl <antti.ollila@demeca.fi>
2025-11-12 20:55:51 +0200

    Week3 ready

--- Week3/lue_varaukset.py
+++ Week3/lue_varaukset.py
@@ -1,72 +1,52 @@
-"""
-Ohjelma joka lukee tiedostossa olevat varaustiedot
-ja tulostaa ne konsoliin käyttäen funkitoita.
-Alla esimerkkitulostus:
 
-Varausnumero: 123
-Varaaja: Anna Virtanen
-Päivämäärä: 31.10.2025
-Aloitusaika: 10.00
-Tuntimäärä: 2
-Tuntihinta: 19,95 €
-Kokonaishinta: 39,9 €
-Maksettu: Kyllä
-Kohde: Kokoustila A
-Puhelin: 0401234567
-Sähköposti: anna.virtanen@example.com
-
-"""
 from datetime import datetime
 
 def hae_varausnumero(varaus):
-    nro = varaus[0]
+    nro = int(varaus[0])
     print(f"Varausnumero: {nro}")
+    return nro
 def hae_varaaja(varaus):
-    nimi = varaus[1]
+    nimi = str(varaus[1])
     print(f"Varaaja: {nimi}")
+    return nimi
 def hae_paiva(varaus):
-    pva = varaus[2]
+    pva = str(varaus[2])
     print(f"Varauspäivä: {pva}")
+    return pva
 def hae_aloitusaika(varaus):
-    aika = varaus[3]
+    aika = str(varaus[3])
     print(f"Aloitusaika: {aika}")
+    return aika
 def hae_tuntimaara(varaus):
-    maara = varaus[4]
+    maara = int(varaus[4])
     print(f"Varausnumero: {maara}")
+    return maara
 def hae_tuntihinta(varaus):
-    hinta = varaus[5]
-    print(f"Tuntihinta: {hinta}")
+    hinta = float(varaus[5])
+    print(f"Tuntihinta: {hinta} €")
+    return hinta
 def hae_maksettu(varaus):
-    maks = varaus[6]
+    maks = bool(varaus[6])
     print(f"Varaus maksettu: {maks}")
+    return maks
 def hae_kohde(varaus): 
-    kohde = varaus[7]
+    kohde = str(varaus[7])
     print(f"Varauskohde: {kohde}")
+    return kohde
 def hae_puhelin(varaus): 
-    puh = varaus[8]
+    puh = int(varaus[8])
     print(f"Puhelinnumero: {puh}")
+    return puh
 def hae_sahkoposti(varaus):
-    sposti = varaus[9]
+    sposti = str(varaus[9])
     print(f"Sähköposti: {sposti}")
+    return sposti
 def laske_kokonaishinta(varaus):
-    yht = varaus[4]*varaus[5]
-    print(f"Hinta yhteensä: {yht}")
+    yht = float(varaus[4])*float(varaus[5])
+    print(f"Hinta yhteensä: {yht} €")
+    return yht
 
-#def tulosta_varaus(varaus)
-
-def main():
-    # Maaritellaan tiedoston nimi suoraan koodissa
-    varaukset = "varaukset.txt"
-
-    # Avataan tiedosto, luetaan ja splitataan sisalto
-    with open(varaukset, "r", encoding="utf-8") as f:
-        varaus = f.read().strip()
-        varaus = varaus.split('|')
-
-    # Toteuta loput funktio hae_varaaja(varaus) mukaisesti
-    # Luotavat funktiota tekevat tietotyyppien muunnoksen
-    # ja tulostavat esimerkkitulosteen mukaisesti
-
+def tulosta_varaus(varaus): 
     hae_varausnumero(varaus)
     hae_varaaja(varaus)
     hae_paiva(varaus)
@@ -78,6 +58,18 @@ def main():
     hae_kohde(varaus)
     hae_puhelin(varaus)
     hae_sahkoposti(varaus)
+    print()
+
+def main():
+    # Maaritellaan tiedoston nimi suoraan koodissa
+    varaukset = "varaukset.txt"
+
+    # Avataan tiedosto, luetaan, splitataan ja tulostetaan sisalto
+    with open(varaukset, "r", encoding="utf-8") as f:
+        for line in f:
+            varaus = line.strip()
+            varaus = varaus.split('|')
+            tulosta_varaus(varaus)
 
 if __name__ == "__main__":
     main()
